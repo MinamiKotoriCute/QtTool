aa2410af078282d296feacf99a93dd8fc3afc48f
joeyzeng <MinamiKotoriCute@gmail.com>
2019-09-14 23:46:10 +0800

    Fixed too new function

--- tool/http_server/http_response.cpp
+++ tool/http_server/http_response.cpp
@@ -25,8 +25,8 @@ QByteArray HttpResponse::Data() const
 {
     QByteArray data;
     data += "HTTP/1.1 " + status_code_ + "\r\n";
-    for(auto it = header_group_.constKeyValueBegin(); it != header_group_.constKeyValueEnd(); ++it) {
-        data += (*it).first + ": " + (*it).second + "\r\n";
+    for(auto it = header_group_.constBegin(); it != header_group_.constEnd(); ++it) {
+        data += it.key() + ": " + it.value() + "\r\n";
     }
     data += "\r\n" + body_;
     return data;
